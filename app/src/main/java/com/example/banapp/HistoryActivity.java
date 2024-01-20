f04ef989ef08fb477f29f078b295aed8300bb495
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2024-01-20 21:39:33 +0900

    ヒストリー画面を動的に修正

--- app/src/main/java/com/example/banapp/HistoryActivity.java
+++ app/src/main/java/com/example/banapp/HistoryActivity.java
@@ -1,27 +1,110 @@
 package com.example.banapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.example.banapp.model.History;
+import com.example.banapp.model.Item;
+import com.example.banapp.model.Pet;
+import com.example.banapp.model.User;
+import com.example.banapp.repository.PetRepository;
+import com.example.banapp.repository.UserRepository;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        private History history;
 
-        //ボタンを押した時に画面遷移（ボタンIDは後で変える）
-        findViewById(R.id.btHistoryBack).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HistoryActivity.this, HomeActivity.class);
-                startActivity(intent);
+        private Pet pet;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_history);
+
+//
+//            // ユーザの取得
+//            UserRepository.getUserById(getUserId(), getedUser -> {
+//                user = getedUser;
+//                if (user != null) {
+//                    tvCoin.setText(String.valueOf(user.getCoin()));
+//                }
+//            });
+//
+//            // ペット取得
+//            PetRepository.getPetById(getPetId(), getedPet -> {
+//                pet = getedPet;
+//                if (pet != null) {
+//                    tvPetName.setText(pet.getName());   // ペットの名前を表示
+//                }
+//            });
+
+            final String[] pets={"たろう","じろう","さぶろう","たろうまる","たろうたろう","あいうえお"};
+            final int[] days={1,2,3,4,5,6};
+            final int[] money={100,200,300,400,500,600};
+
+
+            // ペットごとにTextViewを作成
+            LinearLayout historyLayout = findViewById(R.id.linearLayoutHistory);
+
+            for(String pet:pets) {
+                addTextView(historyLayout,pets[i],days[i],money[i]);
+
             }
-        });
+
+//            Item.getItems(histories -> {
+//                runOnUiThread(() -> {
+//                    for (History history : histories) {
+//                        int imageResourceId = getImageResourceId(item.getName());
+//                        addTextView(historyLayout, history);
+//                    }
+//                });
+//            });
+
+            //戻る処理
+//            findViewById(R.id.btBackFrom).setOnClickListener(view -> navigateToHome());
+        }
+
+        private void addTextView(LinearLayout layout,final String pet,final int day,final int money) {
+            // TextViewを生成
+            TextView textView = new TextView(this);
+            textView.setText("ペットの名前: " + pet + "\n生存期間: " + day + "日\n得したお金: " + money + "円");
 
 
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            textView.setPadding(20,0,20,0);
+
+//            // マージンの設定
+//            params.setMargins(0, 0, 0, 32);
+
+
+            // ボタンにレイアウトパラメータを設定
+            textView.setLayoutParams(params);
+
+            //ボタンの色設定
+            textView.setBackgroundResource(R.drawable.shape_style_white);
+
+            // LinearLayoutにボタンを追加
+            layout.addView(textView);
+        }
+
+
+
+
+        private int getUserId() {
+            SharedPreferences sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE);
+            return sharedPreferences.getInt("userId", -1);
+        }
     }
 }
