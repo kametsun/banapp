0cd46e640fd58ac1cf5816386039f29746e4b2ae
kametsun <san-j22011@sist.ac.jp>
2024-01-20 17:28:04 +0900

    fix Shop画面

--- app/src/main/java/com/example/banapp/ShopActivity.java
+++ app/src/main/java/com/example/banapp/ShopActivity.java
@@ -1,21 +1,37 @@
 package com.example.banapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.banapp.model.Item;
+import com.example.banapp.model.User;
+import com.example.banapp.repository.UserRepository;
 
 public class ShopActivity extends AppCompatActivity {
+    private User user;
+    private TextView tvCoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        tvCoin = findViewById(R.id.tvCoin);
+
+        // ユーザの取得
+        UserRepository.getUserById(getUserId(), getedUser -> {
+            user = getedUser;
+            if (user != null) {
+                tvCoin.setText(String.valueOf(user.getCoin()));
+            }
+        });
 
         // 商品ごとにボタンを作成
         LinearLayout buttonsLayout = findViewById(R.id.bt_products);
@@ -24,43 +40,19 @@ public class ShopActivity extends AppCompatActivity {
             runOnUiThread(() -> {
                 for (Item item : items) {
                     int imageResourceId = getImageResourceId(item.getName());
-                    addButton(buttonsLayout, item.getName(), item.getEnergy(), item.getPrice(), imageResourceId);
+                    addButton(buttonsLayout, imageResourceId, item);
                 }
             });
         });
 
         //戻る処理
-        findViewById(R.id.bt_Back_shop).setOnClickListener(view -> {
-            Intent intent = new Intent(ShopActivity.this, HomeActivity.class);
-
-            startActivity(intent);
-        });
+        findViewById(R.id.bt_Back_shop).setOnClickListener(view -> navigateToHome());
     }
 
-    private int getImageResourceId(String name) {
-        switch (name) {
-            case "クロワッサン":
-                return R.drawable.croissant;
-
-            case "ハンバーガー":
-                return R.drawable.hamburger;
-
-            case "アイスクリーム":
-                return R.drawable.icecream;
-
-            case "ラーメン":
-                return R.drawable.ramen;
-
-            case "ケーキ":
-                return R.drawable.cake;
-        }
-        return -1;
-    }
-
-    private void addButton(LinearLayout layout, final String product, final int energy, final int price, int imageResourceId) {
+    private void addButton(LinearLayout layout, int imageResourceId, Item item) {
         // ボタンを生成
         AppCompatButton button = new AppCompatButton(this);
-        button.setText("商品名: " + product + "\nエネルギー: " + energy + "\n金額: " + price + "円");
+        button.setText("商品名: " + item.getName() + "\nエネルギー: " + item.getEnergy() + "\n金額: " + item.getPrice() + "円");
 
         // 画像の大きさを調整
         Drawable drawable = getResources().getDrawable(imageResourceId);
@@ -94,5 +86,36 @@ public class ShopActivity extends AppCompatActivity {
 
         // LinearLayoutにボタンを追加
         layout.addView(button);
+    }
+
+    private int getImageResourceId(String name) {
+        switch (name) {
+            case "クロワッサン":
+                return R.drawable.croissant;
+
+            case "ハンバーガー":
+                return R.drawable.hamburger;
+
+            case "アイスクリーム":
+                return R.drawable.icecream;
+
+            case "ラーメン":
+                return R.drawable.ramen;
+
+            case "ケーキ":
+                return R.drawable.cake;
+            default:
+                return R.drawable.hatena;
+        }
+    }
+
+    private void navigateToHome() {
+        Intent intent = new Intent(ShopActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private int getUserId() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE);
+        return sharedPreferences.getInt("userId", -1);
     }
 }
