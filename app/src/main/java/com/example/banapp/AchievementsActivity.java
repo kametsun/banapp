4215719a50a3d2491cad92eb351df4f59d6daee6
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2024-01-20 23:13:45 +0900

    アチーブメント　達成したかの判定処理 灰色→ピンク色にする処理を追加 ボタンを押せる→押せないようにする処理を追加

--- app/src/main/java/com/example/banapp/AchievementsActivity.java
+++ app/src/main/java/com/example/banapp/AchievementsActivity.java
@@ -3,6 +3,7 @@ package com.example.banapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,48 @@ public class AchievementsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //仮の変数（実際にはDBから取得した値を格納する）
+        //連続餌やり日数
+        int continueDays=5;
+        //総餌やり日数
+        int totalCount=6;
+
+        //餌やりをしてみようのボタン
+        Button btAchievementsFeed1= findViewById(R.id.btAchievementsFeed1);
+
+        //3日連続餌やりをしようのボタン
+        Button btAchievementsFeedDay3= findViewById(R.id.btAchievementsFeedDay3);
+
+        //5日連続餌やりをしようのボタン
+        Button btAchievementsFeedDay5= findViewById(R.id.btAchievementsFeedDay5);
+
+        //1週間餌やりをしようのボタン
+        Button btAchievementsFeedWeek1= findViewById(R.id.btAchievementsFeedWeek1);
+
+
+        //餌やりをしてみよう
+        if(0<totalCount){
+            btAchievementsFeed1.setBackgroundResource(R.color.pinkButton);//ボタンの色をピンク色にする
+            btAchievementsFeed1.setEnabled(true);//ボタンを押せるようにする
+        }
+        //3日連続餌やりをしよう
+        if(3<=continueDays){
+            btAchievementsFeedDay3.setBackgroundResource(R.color.pinkButton);//ボタンの色をピンク色にする
+            btAchievementsFeedDay3.setEnabled(true);//ボタンを押せるようにする
+        }
+        //5日連続餌やりをしよう
+        if(5<=continueDays){
+            btAchievementsFeedDay5.setBackgroundResource(R.color.pinkButton);//ボタンの色をピンク色にする
+            btAchievementsFeedDay5.setEnabled(true);//ボタンを押せるようにする
+        }
+
+        //1週間餌やりをしよう
+        if(7<=totalCount){
+            btAchievementsFeedWeek1.setBackgroundResource(R.color.pinkButton);//ボタンの色をピンク色にする
+            btAchievementsFeedWeek1.setEnabled(true);//ボタンを押せるようにする
+        }
+
 //        //ボタンを押した時にヒストリー画面へ遷移（ボタン名は仮の名前のため後で変える）
 //        findViewById(R.id.bt_test_history).setOnClickListener(new View.OnClickListener() {
 //            @Override
