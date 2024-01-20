ce19d113e9b5663f57939c3398ac2ced3d5a4044
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2024-01-20 13:34:06 +0900

    History v1 画面完成

--- app/src/main/java/com/example/banapp/HistoryActivity.java
+++ app/src/main/java/com/example/banapp/HistoryActivity.java
@@ -1,4 +1,27 @@
 package com.example.banapp;
 
-public class HistoryActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        //ボタンを押した時に画面遷移（ボタンIDは後で変える）
+//        findViewById(R.id.bt_test_home).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(HistoryActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+
+    }
 }
