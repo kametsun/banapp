20111c29d53bc791c0cc03ffcda40bd610ab279e
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2024-01-21 12:11:40 +0900

    Edittext→textview textviewの中央に文字を配置するように設定

--- app/src/main/java/com/example/banapp/UserInfoActivity.java
+++ app/src/main/java/com/example/banapp/UserInfoActivity.java
@@ -1,8 +1,10 @@
 package com.example.banapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,24 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+//        private int getUserId() {
+//            SharedPreferences sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE);
+//            return sharedPreferences.getInt("userId", -1);
+//        }
+
+
+        //ユーザ名のTextView
+        TextView tvUserNameDisplay= findViewById(R.id.tvUserNameDisplay);
+        tvUserNameDisplay.setText("aa");
+
+        //たばこの値段のTextView
+        TextView tvUserPriceDisplay= findViewById(R.id.tvUserPriceDisplay);
+        tvUserPriceDisplay.setText("bb");
+
+        //たばこの本数のTextView
+        TextView tvUserNumDisplay= findViewById(R.id.tvUserNumDisplay);
+        tvUserNumDisplay.setText("vv");
 
         //戻る処理
         findViewById(R.id.btBackFromAchievevement).setOnClickListener(new View.OnClickListener() {
